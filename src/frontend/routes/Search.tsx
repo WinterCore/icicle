9e69fd33a14a09555cb58036e03223e3900ffb02
WinterCore <hogobbl@gmail.com>
2019-06-28 20:39:27 +0200

    Added pagination

--- src/frontend/routes/Search.tsx
+++ src/frontend/routes/Search.tsx
@@ -1,38 +1,96 @@
 import { parse } from "query-string";
 import * as React from "react";
 import { RouteChildrenProps } from "react-router";
-import { SEARCH } from "../api";
+import Axios from "axios";
 import Error from "../components/Error";
 import Video from "../components/Video";
-import useApi from "../hooks/use-api";
 import Loader from "../icons/Loader";
 
+import api, { SEARCH } from "../api";
 
-
+const PLAYER_HEIGHT = 100;
 
 
 type Video = {
-    id        : string,
-    title     : string,
-    thumbnail : string,
-    duration  : number
-}
+    id        : string;
+    title     : string;
+    thumbnail : string;
+    duration  : number;
+};
 
 const Search: React.FunctionComponent<RouteChildrenProps> = (props) => {
 
-    const { location : { search } } = props;
-    const { q } = parse(search);
-    const { data : responseData, isLoading, error } = useApi({ ...SEARCH(), params : { q } }, [q]);
+    const { location : { search } }         = props;
+    const { q }                             = parse(search);
+    const [nextPageToken, setNextPageToken] = React.useState(null);
+    const [isLoadingMore, setIsLoadingMore] = React.useState(false);
+    const [data, setData]                   = React.useState(null);
+    const [isLoading, setIsLoading]         = React.useState(true);
+    const [error, setError]                 = React.useState(null);
+
+    const containerRef = React.createRef<HTMLDivElement>();
+
+
+    // TODO: refactor the api calls into a hook
+
+    const loadMore = () => {
+        setIsLoadingMore(true);
+        api({
+            ...SEARCH(),
+            params : { q, nextPageToken }
+        }).then((response) => {
+            setData([...data, ...response.data.data]);
+            setNextPageToken(response.data.nextPageToken);
+            setIsLoadingMore(false);
+        }).catch((err) => {
+            console.log(err);
+            setIsLoadingMore(false);
+            // setError(true);
+        });
+    };
+
+    const updatePosition = () => {
+        const { height } = containerRef.current.getBoundingClientRect();
+        if (!isLoading && !isLoadingMore && nextPageToken && (height - window.scrollY + 100) < window.innerHeight)
+            loadMore();
+    };
+
+    React.useEffect(() => {
+        window.addEventListener("scroll", updatePosition);
+        return () => window.removeEventListener("scroll", updatePosition);
+    });
+
+    React.useEffect(() => {
+        const cancelTokenSource = Axios.CancelToken.source();
+        api({
+            ...SEARCH(),
+            params : { q },
+            cancelToken : cancelTokenSource.token
+        }).then((response) => {
+            setData(response.data.data);
+            setNextPageToken(response.data.nextPageToken);
+            setIsLoading(false);
+        }).catch((err) => {
+            console.log(err);
+            setError(true);
+            setIsLoading(false);
+        });
+        return () => cancelTokenSource.cancel("CANCELED");
+    }, [q]);
+
     if (isLoading) return <div className="flex-middle"><Loader /></div>;
     if (error) return <div className="flex-middle"><Error /></div>;
-    const { nextPageToken, data: videos } = responseData;
+
     return (
-        <div className="search-grid row">
-            { videos.map((item: Video) => (
+        <div ref={ containerRef } className="search-grid row">
+            { data.map((item: Video) => (
                 <div key={ item.id } className="video-outer col-xs-12 col-sm-6 col-md-4 col-lg-4">
                     <Video { ...item } />
                 </div>
             )) }
+            <div className="flex-middle col-xs-12">
+                { isLoadingMore && <Loader /> }
+            </div>
         </div>
     );
 }
