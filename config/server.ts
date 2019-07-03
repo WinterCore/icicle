98033cd481b3173f26b3e21251369ea22d4a0db5
WinterCore <hogobbl@gmail.com>
2019-07-03 19:18:58 +0200

    Fixed bug

--- config/server.ts
+++ config/server.ts
@@ -10,7 +10,7 @@ const dbCred = dbUser.length > 0 || dbPass.length > 0 ? `${dbUser}:${dbPass}@` :
 
 export const mongodbServerUrl = process.env.DB_URL || `mongodb://${dbCred}${dbHost}:${dbPort}/${dbName}${dbCred ? "?authSource=admin" : ""}`;
 
-export const DOMAIN = process.env.DOMAIN || "http://localhost:8081";
+export const DOMAIN = process.env.NODE_ENV === "development" ? "http://localhost:8081" : "https://icicle.wintercore.dev";
 export const GOOGLE_CONFIG = {
   clientId: "678441941517-30jpq0673jv9gqmm4n7tm8nsv4304acq.apps.googleusercontent.com",
   clientSecret: "RjG-37S3rIOSYrTissbf5xch",
@@ -20,4 +20,5 @@ export const GOOGLE_CONFIG = {
 export const JWT_SECRET = process.env.JWT_SECRET || "NYAN_CAT";
 export const AUDIO_PATH = "public/audio";
 
-export const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || "";
+// export const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || "AIzaSyBJs5mQIA9dK72XOvTc6mi5kbV9YrLMBjw";
+export const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || "AIzaSyDfXXP1CXawPt6Pwb0EQRIxbAfMu2NVkjU";
