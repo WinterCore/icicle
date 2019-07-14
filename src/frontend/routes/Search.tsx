363f65ea96d45d6eacd5913edace21f6ca33c468
WinterCore <hogobbl@gmail.com>
2019-07-14 05:56:30 +0200

    Make 4 videos appear per row on larger screens

--- src/frontend/routes/Search.tsx
+++ src/frontend/routes/Search.tsx
@@ -85,7 +85,7 @@ const Search: React.FunctionComponent<RouteChildrenProps> = (props) => {
     return (
         <div ref={ containerRef } className="search-grid row">
             { data.map((item: Video) => (
-                <div key={ item.id } className="video-outer col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                <div key={ item.id } className="video-outer col-xs-12 col-sm-6 col-md-4 col-lg-3">
                     <Video { ...item } />
                 </div>
             )) }
