3915ac84d2c6ca77aabcb80130c0a88f11a60646
WinterCore <hogobbl@gmail.com>
2019-07-15 01:00:15 +0200

    Removed youtube api key

--- config/server.ts
+++ config/server.ts
@@ -20,5 +20,4 @@ export const GOOGLE_CONFIG = {
 export const JWT_SECRET = process.env.JWT_SECRET || "NYAN_CAT";
 export const AUDIO_PATH = "public/audio";
 
-// export const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || "AIzaSyBJs5mQIA9dK72XOvTc6mi5kbV9YrLMBjw";
-export const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || "AIzaSyDfXXP1CXawPt6Pwb0EQRIxbAfMu2NVkjU";
+export const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
