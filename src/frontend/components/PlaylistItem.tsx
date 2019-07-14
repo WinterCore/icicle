da3f02e7efcd0ab6c440c11f37177edf7ecb0770
WinterCore <hogobbl@gmail.com>
2019-07-14 07:52:38 +0200

    Added add to queue button

--- src/frontend/components/PlaylistItem.tsx
+++ src/frontend/components/PlaylistItem.tsx
@@ -3,23 +3,28 @@ import * as React from "react";
 import CrossIcon  from "../icons/Cross";
 import PlayIcon   from "../icons/Play";
 import LoaderIcon from "../icons/Loader";
+import WatchIcon  from "../icons/Watch";
 
 import { secondsToTime } from "../helpers";
 
 import { usePlayer }       from "../contexts/player";
 import { useNotification } from "../contexts/notification";
+import { useUser } from "../contexts/user";
 
 import TextRoller from "./TextRoller";
 
-import api, { DELETE_PLAYLIST_ITEM } from "../api";
+import api, { DELETE_PLAYLIST_ITEM, ADD_TO_QUEUE } from "../api";
 
 
 const PlaylistItem: React.FunctionComponent<PlaylistItemProps> = (props) => {
     const { thumbnail, title, duration, videoId, playlistId, onDelete } = props;
 
-    const { startStream }           = usePlayer();
-    const [isLoading, setIsLoading] = React.useState<boolean>(false);
-    const { addNotification }       = useNotification();
+    const [isLoading, setIsLoading]                     = React.useState<boolean>(false);
+    const [isAddToQueueLoading, setIsAddToQueueLoading] = React.useState<boolean>(false);
+
+    const { startStream, nowPlaying } = usePlayer();
+    const { user }                    = useUser();
+    const { addNotification }         = useNotification();
 
     const deletePlaylistItem = () => {
         setIsLoading(true);
@@ -39,6 +44,43 @@ const PlaylistItem: React.FunctionComponent<PlaylistItemProps> = (props) => {
         });
     };
 
+    const addToQueue = async (e) => {
+        e.preventDefault();
+        setIsAddToQueueLoading(true);
+        try {
+            if (!nowPlaying || nowPlaying.by._id !== user._id) { // Start a stream if the user is not already in one or if the current stream is not the user's
+                startStream(videoId);
+                addNotification({
+                    id :`${Date.now()}`,
+                    message : "Your queue is empty, the video will play immediately.",
+                    type : "success",
+                    time : 5000
+                });
+            } else {
+                await api({ ...ADD_TO_QUEUE(), data : { id : videoId } });
+                addNotification({
+                    id :`${Date.now()}`,
+                    message : `${title} has been added to the queue.`,
+                    type : "success",
+                    time : 5000
+                });
+            }
+            setIsAddToQueueLoading(false);
+        } catch(e) {
+            console.log(e);
+            
+            setIsAddToQueueLoading(false);
+            if (e.response && e.response.status === 422) {
+                addNotification({
+                    id :`${Date.now()}`,
+                    message : e.response.data.errors,
+                    type : "error",
+                    time : 5000
+                });
+            }
+        }
+    };
+
     const playPlaylistItem = () => {
         startStream(videoId);
     };
@@ -54,7 +96,6 @@ const PlaylistItem: React.FunctionComponent<PlaylistItemProps> = (props) => {
                                 { title }
                             </TextRoller>
                         </div>
-                        {/* <div className="song-time-remaining washed-out">Plays in 5 mins</div> */}
                     </div>
                 </div>
                 <div className="song-duration">
@@ -62,7 +103,8 @@ const PlaylistItem: React.FunctionComponent<PlaylistItemProps> = (props) => {
                 </div>
                 {
                     <div className="song-actions">
-                        { <PlayIcon onClick={ playPlaylistItem } /> }
+                        { isAddToQueueLoading ? <LoaderIcon /> : <WatchIcon onClick={ addToQueue } /> }
+                        <PlayIcon onClick={ playPlaylistItem } />
                         { isLoading ? <LoaderIcon /> : <CrossIcon onClick={ deletePlaylistItem } /> }
                     </div>
                 }
