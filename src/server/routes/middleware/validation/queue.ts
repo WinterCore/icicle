ac9d76cb088e3b0ed9401de1ba5bfe12587200b0
WinterCore <hogobbl@gmail.com>
2019-08-24 20:27:05 +0200

    Fixes #20

--- src/server/routes/middleware/validation/queue.ts
+++ src/server/routes/middleware/validation/queue.ts
@@ -8,9 +8,4 @@ export default function validateQueue(req: Request, res: Response, next) {
     const { id } = req.body;
     if (!id) return next(new ValidationError(["The id parameter is required"]));
     next();
-    // Queue.countDocuments({ by : req.userId, videoId : id })
-    //     .then((count) => {
-    //         if (count > 0) return next(new ValidationError("The video is already in the queue"));
-    //         else next();
-    //     }).catch(next);
 }
