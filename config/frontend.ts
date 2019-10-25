9b1e55904adf9f11517fbcc0e6c964f4378e1c05
WinterCore <hogobbl@gmail.com>
2019-10-26 01:22:39 +0200

    Changed domain name

--- config/frontend.ts
+++ config/frontend.ts
@@ -1,4 +1,4 @@
 import { DOMAIN as SERVER_DOMAIN } from "./server";
 export const API_URL = `${SERVER_DOMAIN}/api`;
-export const DOMAIN =  process.env.NODE_ENV === "development" ? "https://icicle.dev" : "https://icicle.wintercore.dev";
+export const DOMAIN =  process.env.NODE_ENV === "development" ? "https://icicle.dev" : "https://icicle.media";
 // TODO: Add api key in the future
