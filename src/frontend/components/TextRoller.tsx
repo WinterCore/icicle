98fb8a7973dba82ad87e062cefbb0ddb457cbb8d
WinterCore <hogobbl@gmail.com>
2019-10-25 04:04:40 +0200

    Only start rolling the text if the user hovers over the text, and do it only once

--- src/frontend/components/TextRoller.tsx
+++ src/frontend/components/TextRoller.tsx
@@ -1,21 +1,32 @@
 import * as React from "react";
 
-
-
 const TextRoller: React.FunctionComponent<TextRollerProps> = ({ children, speed = 0.4, uid = 1 }) => {
     const containerRef    = React.useRef<HTMLDivElement>(null);
     const itemRef         = React.useRef<HTMLDivElement>(null);
 
     React.useLayoutEffect(() => {
-        let textWidth: number, containerWidth: number, tempLeft = 0, tracker: number;
+        let textWidth: number, containerWidth: number, tempLeft = 0, tracker: number = 0, cycle = 1;
         const loop = () => {
-            if (textWidth + tempLeft < 0) tempLeft = containerWidth
+            if (tempLeft > 0 && tempLeft - speed < 0) {
+                cycle += 1;
+            }
+            if (textWidth + tempLeft < 0) tempLeft = containerWidth;
             tempLeft -= speed;
             itemRef.current!.style.transform = `translateX(${tempLeft - speed}px)`;
-            tracker = window.requestAnimationFrame(loop);
+            if (cycle === 2) {
+                window.cancelAnimationFrame(tracker);
+                tracker = 0;
+                itemRef.current!.style.transform = `translateX(0px)`;
+            } else {
+                tracker = window.requestAnimationFrame(loop);
+            }
         };
         
         const init = () => {
+            if (tracker !== 0) {
+                return;
+            }
+            cycle = 1;
             window.cancelAnimationFrame(tracker);
             containerWidth = containerRef.current!.getBoundingClientRect().width;
             textWidth      = itemRef.current!.getBoundingClientRect().width;
@@ -25,13 +36,12 @@ const TextRoller: React.FunctionComponent<TextRollerProps> = ({ children, speed 
                 tracker = window.requestAnimationFrame(loop);
             }
         };
-        init();
 
-        window.addEventListener("resize", init);
+        containerRef.current!.addEventListener("mousemove", init);
         
         return () => {
             window.cancelAnimationFrame(tracker);
-            window.removeEventListener("resize", init);
+            containerRef.current!.removeEventListener("mousemove", init);
         };     
     }, [uid]);
 
